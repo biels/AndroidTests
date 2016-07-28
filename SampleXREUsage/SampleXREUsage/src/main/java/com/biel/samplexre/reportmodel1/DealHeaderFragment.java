9036d544c499608496098a561aa61143ab78534c
Biel Simon Rojas <bielsimon2@gmail.com>
2016-07-28 20:30:03 +0200

    Updated DealHeaderFragment

--- SampleXREUsage/SampleXREUsage/src/main/java/com/biel/samplexre/reportmodel1/DealHeaderFragment.java
+++ SampleXREUsage/SampleXREUsage/src/main/java/com/biel/samplexre/reportmodel1/DealHeaderFragment.java
@@ -1,13 +1,15 @@
 package com.biel.samplexre.reportmodel1;
 
 import com.biel.xre.generation.ReportFragment;
+import com.biel.xre.generation.xhtml.Literal;
+import com.biel.xre.generation.xhtml.Tag;
 
 public class DealHeaderFragment extends ReportFragment{
 
 	@Override
 	public String getXHTML() {
 		// TODO Auto-generated method stub
-		return "<h1>Header</h1>";
+		return new Tag("h1", new Literal("Header")).getXHTML();
 	}
 
 }
